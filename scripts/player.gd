4e61594c73bb554d388baadc16cc07762f67cb23
Azmain Ahnaf <azmainahnaf2003@gmail.com>
2024-05-25 11:10:35 +0600

    Implemented acceleration and deceleration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150
+const JUMP_VELOCITY = -200.0
+const ACCELERATION = 20
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,15 +14,18 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("Move_right"):
+		print(velocity.x)
+		velocity.x = min(velocity.x + ACCELERATION, SPEED)
+	elif Input.is_action_pressed("Move_left"):
+		print(velocity.x)
+		velocity.x = max(velocity.x - ACCELERATION, -SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = lerp(velocity.x, 0.0, 0.1)
 	move_and_slide()
+	
