7329b1b1c71313a09bc5a0d4b233c796c2b4084f
Azmain Ahnaf <azmainahnaf2003@gmail.com>
2024-06-05 17:39:53 +0600

    yoyo

--- scenes/prologue.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-
-
-func _on_button_pressed():
-	get_tree().quit()
--- scripts/small_box.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody2D
-class_name MovableBody
-
-func _integrate_forces(_state: PhysicsDirectBodyState2D):
-	angular_velocity = 0
-	rotation = 0
